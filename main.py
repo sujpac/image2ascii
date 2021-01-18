c4df8c6cd4b8be8793a4e348a72ea40bf070fa68
sujpac <sujit.pack@gmail.com>
2021-01-18 03:07:59 -0800

    add code to represent image as 2d pixel matrix

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ from PIL import Image
 
 img = Image.open('plane.jpg')
 print('Successfully loaded image!')
-print('Image size: {} x {}'.format(*img.size))
+print('Image size: {} x {}'.format(*img.size))  # 640x480
+
+width, height = img.size
+pixels = list(img.getdata())
+pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+print('Successfully constructed pixel matrix!')
+print('Pixel matrix size: {} x {}'.format(len(pixels[0]), len(pixels)))
+print('First 10 cells in matrix:\n', pixels[0][:10])
